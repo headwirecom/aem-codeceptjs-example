8769a940c36dc87035310e4e109ec855156929a8
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-01-26 16:43:19 +0100

    Use double quotes for literal strings

--- src/test/administering_pages.js
+++ src/test/administering_pages.js
@@ -1,24 +1,24 @@
-Feature('Administering pages in AEM');
+Feature("Administering pages in AEM");
 
-Scenario('Enter the Sites', (I) => {
-	I.amOnPage('http://localhost:4502');
+Scenario("Enter the Sites", (I) => {
+	I.amOnPage("http://localhost:4502");
 
-	I.see('Sign In', '#sign-in-title');
-	I.fillField('#username', 'admin');
-	I.fillField('#password', 'admin');
-	I.pressKey('Enter');
+	I.see("Sign In", "#sign-in-title");
+	I.fillField("#username", "admin");
+	I.fillField("#password", "admin");
+	I.pressKey("Enter");
 
-	I.see('Projects', '.globalnav-homecard-title');
-	I.see('Sites', '.globalnav-homecard-title');
-	I.see('Experience Fragments', '.globalnav-homecard-title');
-	I.see('Assets', '.globalnav-homecard-title');
-	I.see('Forms', '.globalnav-homecard-title');
-	I.see('Screens', '.globalnav-homecard-title');
-	I.see('Personalization', '.globalnav-homecard-title');
-	I.see('Commerce', '.globalnav-homecard-title');
-	I.see('Communities', '.globalnav-homecard-title');
+	I.see("Projects", ".globalnav-homecard-title");
+	I.see("Sites", ".globalnav-homecard-title");
+	I.see("Experience Fragments", ".globalnav-homecard-title");
+	I.see("Assets", ".globalnav-homecard-title");
+	I.see("Forms", ".globalnav-homecard-title");
+	I.see("Screens", ".globalnav-homecard-title");
+	I.see("Personalization", ".globalnav-homecard-title");
+	I.see("Commerce", ".globalnav-homecard-title");
+	I.see("Communities", ".globalnav-homecard-title");
 
-	I.click({'xpath': '//*[@id="globalnav-start-home-collection"]/coral-masonry-item[2]'});
+	I.click({"xpath": "//*[@id="globalnav-start-home-collection"]/coral-masonry-item[2]"});
 
 	pause();
 });
