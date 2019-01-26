c7a67605d14efb0cd09b6b410a744fa4951efaa4
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-01-26 18:24:02 +0100

    Clean up code duplication

--- src/main/custom_steps.js
+++ src/main/custom_steps.js
@@ -1,11 +1,15 @@
 module.exports = function() {
   return actor({
 
-	amOnAuthor: function() {
-		this.amOnPage("http://localhost:4502");
+	amOnAuthor: function(path = "") {
+		if (!path.startsWith("/")) {
+			path = "/" + path;
+		}
+
+		this.amOnPage("http://localhost:4502" + path);
 	},
 
-	signIn: function(username, password) {
+	signIn: function(username = "admin", password = "admin") {
 		this.see("Sign In", "#sign-in-title");
 		this.fillField("#username", username);
 		this.fillField("#password", password);
--- src/test/administering_pages.js
+++ src/test/administering_pages.js
@@ -1,18 +1,17 @@
 Feature("Administering pages in AEM");
 
-Scenario("Enter the Sites", (I) => {
+Scenario("Enter AEM Start", (I) => {
 	I.amOnAuthor();
-	I.signIn("admin", "admin");
+	I.signIn();
 
-	I.see("Projects", ".globalnav-homecard-title");
-	I.see("Sites", ".globalnav-homecard-title");
-	I.see("Experience Fragments", ".globalnav-homecard-title");
-	I.see("Assets", ".globalnav-homecard-title");
-	I.see("Forms", ".globalnav-homecard-title");
-	I.see("Screens", ".globalnav-homecard-title");
-	I.see("Personalization", ".globalnav-homecard-title");
-	I.see("Commerce", ".globalnav-homecard-title");
-	I.see("Communities", ".globalnav-homecard-title");
-
-	I.click({"xpath": "//*[@id='globalnav-start-home-collection']/coral-masonry-item[2]"});
+	let locator = ".globalnav-homecard-title";
+	I.see("Projects", locator);
+	I.see("Sites", locator);
+	I.see("Experience Fragments", locator);
+	I.see("Assets", locator);
+	I.see("Forms", locator);
+	I.see("Screens", locator);
+	I.see("Personalization", locator);
+	I.see("Commerce", locator);
+	I.see("Communities", locator);
 });
