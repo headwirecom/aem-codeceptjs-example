40f1ab2ca15d783338aaa3312723b6ac2af92e39
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-01-30 00:04:49 +0100

    Remove 'function' keyword

--- src/main/custom_steps.js
+++ src/main/custom_steps.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return actor({
 
-	amOnAuthor: function(path = "") {
+	amOnAuthor(path = "") {
 		if (!path.startsWith("/")) {
 			path = "/" + path;
 		}
@@ -9,31 +9,31 @@ module.exports = function() {
 		this.amOnPage("http://localhost:4502" + path);
 	},
 
-	type: function(text) {
+	type(text) {
 		for (var i = 0; i < text.length; i++) {
 		  this.pressKey(text.charAt(i));
 		}
 	},
 
-	press: function(key, times = 1) {
+	press(key, times = 1) {
 		for (var i = 0; i < times; i++) {
 		  this.pressKey(key);
 		}
 	},
 
-	pressEnter: function(times = 1) {
+	pressEnter(times = 1) {
 		this.press("Enter", times);
 	},
 
-	pressTab: function(times = 1) {
+	pressTab(times = 1) {
 		this.press("Tab", times);
 	},
 
-	pressRight: function(times = 1) {
+	pressRight(times = 1) {
 		this.press("ArrowRight", times);
 	},
 
-	pressDown: function(times = 1) {
+	pressDown(times = 1) {
 		this.press("ArrowDown", times);
 	}
 
