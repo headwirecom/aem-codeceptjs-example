7464d65a248b53b08f76920da429221f1823d53e
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-02-06 23:31:59 +0100

    Use tests page as root

--- src/test/administering_pages.js
+++ src/test/administering_pages.js
@@ -4,12 +4,12 @@ Before(loginPage => {
     loginPage.signIn();
 });
 
-const parentPagePath = "/content/we-retail/language-masters/en";
+const parentPagePath = "/content/we-retail/language-masters/en/tests";
 
 Scenario("Navigate to 'Create Page' wizard", (sitesPage, createPageWizardPage) => {
 	sitesPage.open("/content");
 	sitesPage.switchToListView();
-	sitesPage.navigateListViewTo(["We.Retail", "Language Master", "English"]);
+	sitesPage.navigateListViewTo(["We.Retail", "Language Master", "English", "Test Content Root"]);
 	sitesPage.gotoCreatePageWizard();
 	createPageWizardPage.validate(parentPagePath);
 });
