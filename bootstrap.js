3bca789b1f31baa9281a3e19f5cb47f56532a733
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-02-07 14:13:29 +0100

    Extract pipeZipToPackageFile

--- bootstrap.js
+++ bootstrap.js
@@ -1,27 +1,20 @@
 const fs = require("fs");
 const zipFolderContent = require("./lib/zip.js").zipFolderContent;
-const installPackage = require("./lib/aem.js").installPackage;
+const aemLib = require("./lib/aem.js");
+const project = require("./project.config.js");
 
-let project = require("./project.config.js");
-let contentPath = "./" + project.contentFolder;
-const targetPath = "./" + project.tempFolder;
-const zipPath = targetPath + "/" + project.contentFolder + ".zip";
-
+let targetPath = "./" + project.tempFolder;
 if (!fs.existsSync(targetPath)) {
 	console.log("Creating temporary folder '" + targetPath + "'.");
 	fs.mkdirSync(targetPath, { recursive: true });
 }
 
+let zipPath = targetPath + "/" + project.contentFolder + ".zip";
 console.log("Creating test content package '" + zipPath + "'.");
-let zip = zipFolderContent(contentPath);
-zip.generateNodeStream({
-	type: "nodebuffer",
-	streamFiles: true,
-	compression: "DEFLATE"
-})
-.pipe(fs.createWriteStream(zipPath))
+let zip = zipFolderContent("./" + project.contentFolder);
+aemLib.pipeZipToPackageFile(zip, zipPath)
 .on("finish", function() {
 	let aem = require("./aem.config.js");
 	console.log("Uploading the package to '" + aem.url + "'.");
-	installPackage(aem, zipPath);
+	aemLib.installPackage(aem, zipPath);
 });
