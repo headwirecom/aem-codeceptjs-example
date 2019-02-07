787f4fc3693f79dd8ceb3ee27609a9c46cca7ff0
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-02-07 22:06:21 +0100

    Just follow the docs for hooks
    https://codecept.io/hooks/#bootstrap-teardown

--- bootstrap.js
+++ bootstrap.js
@@ -3,15 +3,19 @@ const zipFolderContent = require("./lib/zip.js").zipFolderContent;
 const aemLib = require("./lib/aem.js");
 const project = require("./project.config.js");
 
-project.ensureTempFolder();
-
-let zipPath = "./" + project.getContentZipFilePath();
-console.log("Creating test content package '" + zipPath + "'.");
-let zip = zipFolderContent("./" + project.contentFolder);
-aemLib.pipeZipToPackageFile(zip, zipPath)
-.on("finish", function() {
-	let aem = require("./aem.config.js");
-	console.log("Uploading the package to '" + aem.url + "'.");
-	aemLib.installPackage(zipPath, aem);
-	fs.unlink(zipPath, (err) => { });
-});
+module.exports = function(done) {
+	project.ensureTempFolder();
+	let zipPath = "./" + project.getContentZipFilePath();
+	console.log("Creating test content package '" + zipPath + "'.");
+	let zip = zipFolderContent("./" + project.contentFolder);
+	aemLib.pipeZipToPackageFile(zip, zipPath)
+	.on("finish", function() {
+		let aem = require("./aem.config.js");
+		console.log("Uploading the package to '" + aem.url + "'.");
+		aemLib.installPackage(zipPath, aem, function() {
+			console.log("Removing test content package.");
+			fs.unlink(zipPath, error => { });
+			done();
+		});
+	});
+}
--- teardown.js
+++ teardown.js
@@ -3,5 +3,10 @@ const aemLib = require("./lib/aem.js");
 const project = require("./project.config.js");
 const aem = require("./aem.config.js");
 
-aemLib.deleteNode(aem, project.tempPagesRoot);
-aemLib.deleteNode(aem, "/etc/packages/" + project.getContentZipFileName());
+module.exports = function(done) {
+	console.log("Deleting package from AEM instance.");
+	aemLib.deleteNode(aem, "/etc/packages/" + project.getContentZipFileName(), function() {
+		console.log("Deleting test content from AEM instance.");
+		aemLib.deleteNode(aem, project.tempPagesRoot, done);
+	});
+}
