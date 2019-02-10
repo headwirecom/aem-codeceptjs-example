33b99451f2f418241b59dd12d8635f9d5733da37
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-02-10 21:12:58 +0100

    Extract versioning.js

--- installer.js
+++ installer.js
@@ -1,31 +1,5 @@
-const fs = require("fs");
 const aem = require("./aem.config.js");
-
-const includes = {};
-
-function fileNameToPageName(name) {
-	const objName = name.split('.')[0];
-	const pageObjName = objName.charAt(0).toLowerCase() + objName.substring(1, objName.length) + "Page";
-	return pageObjName;
-}
-
-function init() {
-	const pagesVersionPath = './src/main/'+aem.version+'/pages';
-
-	const pages = fs.readdirSync('./src/main/pages')
-	const pagesVersion = fs.readdirSync(pagesVersionPath)
-
-	for(let i = 0; i < pagesVersion.length; i++) {
-		includes[fileNameToPageName(pagesVersion[i])] = (pagesVersionPath + "/" + pagesVersion[i])
-	}
-	for(let i = 0; i < pages.length; i++) {
-		if(!pagesVersion[fileNameToPageName(pages[i])]) {
-			includes[fileNameToPageName(pages[i])] = ("./src/main/pages/" + pages[i])
-		}
-	}
-}
-
-init();
+const versioning = require("./lib/versioning.js");
 
 module.exports = {
 	getUrl() {
@@ -33,6 +7,6 @@ module.exports = {
 	},
 
 	getIncludes() {
-		return includes;
+		return versioning.getIncludes();
 	}
 }
