d2bd61ed38b1928959362146bb87975c27965719
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-02-11 19:42:52 +0100

    Unhardcode src/main from lib/require.js

--- project.config.js
+++ project.config.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 module.exports = {
 	aem: "./aem.config.js",
+	sources: "./src/main",
 	contentFolder: "./content",
 	tempFolder: "./target",
 	tempPagesRoot: "/content/we-retail/language-masters/en/tests"
