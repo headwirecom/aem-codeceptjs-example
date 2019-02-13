a838c57a35e4a4aaee67b443f050e9ba1c58342f
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-02-14 00:39:55 +0100

    Add version discovery code

--- bootstrap.js
+++ bootstrap.js
@@ -2,15 +2,19 @@ const fs = require("fs");
 const zipFolderContent = require("./lib/zip.js").zipFolderContent;
 const aemLib = require("./lib/aem.js");
 const project = require("./lib/project.js");
+const aem = project.getAEM();
 
 module.exports = function(done) {
+	aemLib.getVersion(aem, result => {
+		console.log(aem.url + " says that it runs AEM " + result);
+	});
+
 	project.ensureTempFolder();
 	let zipPath = project.getContentZipFilePath();
 	console.log("Creating test content package '" + zipPath + "'.");
 	let zip = zipFolderContent(project.getContentFolderPath());
 	aemLib.pipeZipToPackageFile(zip, zipPath)
 	.on("finish", function() {
-		let aem = project.getAEM();
 		console.log("Uploading the package to '" + aem.url + "'.");
 		aemLib.installPackage(zipPath, aem, function() {
 			console.log("Removing test content package.\n\n");
