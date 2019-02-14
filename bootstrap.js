98c39c1bda222ff3f107e475990c154328697d3e
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2019-02-14 18:59:00 +0100

    Use codeceptjs.outuput instead of console.log

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const output = require('codeceptjs').output;
+
 const zipFolderContent = require("./lib/zip.js").zipFolderContent;
 const aemLib = require("./lib/aem.js");
 const project = require("./lib/project.js");
@@ -6,18 +8,18 @@ const aem = project.getAEM();
 
 module.exports = function(done) {
 	aemLib.getVersion(aem, result => {
-		console.log(aem.url + " says that it runs AEM " + result);
+		output.debug(aem.url + " says that it runs AEM " + result);
 	});
 
 	project.ensureTempFolder();
 	let zipPath = project.getContentZipFilePath();
-	console.log("Creating test content package '" + zipPath + "'.");
+	output.print("Creating test content package '" + zipPath + "'.");
 	let zip = zipFolderContent(project.getContentFolderPath());
 	aemLib.pipeZipToPackageFile(zip, zipPath)
 	.on("finish", function() {
-		console.log("Uploading the package to '" + aem.url + "'.");
+		output.print("Uploading the package to '" + aem.url + "'.");
 		aemLib.installPackage(zipPath, aem, function() {
-			console.log("Removing test content package.\n\n");
+			output.print("Deleting test content package.\n\n");
 			fs.unlink(zipPath, error => { });
 			done();
 		});
--- teardown.js
+++ teardown.js
@@ -1,11 +1,13 @@
+const output = require('codeceptjs').output;
+
 const aemLib = require("./lib/aem.js");
 const project = require("./lib/project.js");
 const aem = project.getAEM();
 
 module.exports = function(done) {
-	console.log("\n\nDeleting package from AEM instance.");
+	output.print("\n\nDeleting package from AEM instance.");
 	aemLib.deleteNode(aem, "/etc/packages/" + project.getContentZipFileName(), function() {
-		console.log("Deleting test content from AEM instance.");
+		output.print("Deleting test content from AEM instance.");
 		aemLib.deleteNode(aem, project.getTempPagesRoot(), done);
 	});
 }
